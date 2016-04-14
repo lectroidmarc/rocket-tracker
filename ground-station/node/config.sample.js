51161ddeabab3968db84b6bcfb15668470b9b0e1
Marc Matteo <lectroidmarc@users.noreply.github.com>
2016-04-14 16:05:10 -0700

    Update config.sample.js

--- ground-station/node/config.sample.js
+++ ground-station/node/config.sample.js
@@ -12,15 +12,15 @@ var config = {
   }
 };
 
-// XBee Series 1 (802.15.4)
-config.xbee.pan_id = [ 0x33, 0x32 ];      // Default: 0x3332
-config.xbee.channel = [ 0x0c ];           // Default: 0x0c
+// for XBee Series 1 (802.15.4) only
+//config.xbee.pan_id = [ 0x33, 0x32 ];      // Default: 0x3332
+//config.xbee.channel = [ 0x0c ];           // Default: 0x0c
 
-// XBee-PRO 900
-config.xbee.pan_id = [ 0x7f, 0xff ];      // Default: 0x7fff
-config.xbee.hopping_channel = [ 0x00 ];   // Default: 0x00
+// for XBee-PRO 900 only
+//config.xbee.pan_id = [ 0x7f, 0xff ];      // Default: 0x7fff
+//config.xbee.hopping_channel = [ 0x00 ];   // Default: 0x00
 
-// XBee ZB only
+// for XBee ZB only only
 config.xbee.pan_id = [ '0x01', '0x23', '0x45', '0x67', '0x89', '0xab', '0xcd', '0xef' ];
 
 module.exports = config;
