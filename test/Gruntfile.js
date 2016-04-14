cf6c2ba1142cb8c51e00286b30ca360537a6dcfb
Marc Matteo <marcm@lectroid.net>
2016-04-14 13:26:05 -0700

    Add the Grunt based dev stuff.

---
+++ test/Gruntfile.js
@@ -0,0 +1,37 @@
+/*
+ *
+ */
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          base: '..',
+          port: 8001,
+          protocol: 'http2',
+          livereload: 35730,
+          open: 'https://localhost:8001'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: {
+          port: 35730,
+          key: grunt.file.read('node_modules/grunt-contrib-connect/tasks/certs/server.key'),
+          cert: grunt.file.read('node_modules/grunt-contrib-connect/tasks/certs/server.crt')
+        },
+        livereloadOnError: false
+      },
+      html: {
+        files: ['index.html', 'components/*.html']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+};
