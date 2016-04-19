26c10d7112275cf2d5f167cab3a19997a9216310
Marc Matteo <marcm@lectroid.net>
2016-04-18 19:56:08 -0700

    More Webapp-y stuff.

--- building/Gruntfile.js
+++ building/Gruntfile.js
@@ -11,7 +11,17 @@ module.exports = function(grunt) {
           port: 8001,
           protocol: 'http2',
           livereload: 35730,
-          open: 'https://localhost:8001'
+          open: 'https://localhost:8001',
+          middleware: function(connect, options, middlewares) {
+            middlewares.unshift(function(req, res, next) {
+              if (req.url === '/manifest.json') {
+                res.setHeader('Content-Type', 'application/manifest+json');
+              }
+              return next();
+            });
+
+            return middlewares;
+          }
         }
       }
     },
