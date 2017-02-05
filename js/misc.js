f8067525bce5b47de9f2a95cd613e121a9d23a7b
Marc Matteo <marcm@lectroid.net>
2017-02-05 12:57:56 -0800

    Fix service worker path.

--- js/misc.js
+++ js/misc.js
@@ -44,7 +44,7 @@ document.querySelector('.show-hidden').onclick = function () {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+    navigator.serviceWorker.register('service-worker.js').then(function (registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function (err) {
       console.log('ServiceWorker registration failed: ', err);
